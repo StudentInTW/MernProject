ccdbd82ec23fdcbb581b96fd43f2a21836b85a2d
Andrew <FakeEmail.com>
2023-06-24 10:06:52 +0800

    Initial commit

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,26 @@
+//註冊、登入、登出都在這裡
+import axios from "axios";
+const API_URL = "http://localhost:8080/api/user";
+class AuthService {
+  login(email, password) {
+    return axios.post(API_URL + "/login", { email, password });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+  //axios post會直接return 一個promise
+  register(username, email, password, role) {
+    return axios.post(API_URL + "/register", {
+      username,
+      email,
+      password,
+      role,
+    });
+  }
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
